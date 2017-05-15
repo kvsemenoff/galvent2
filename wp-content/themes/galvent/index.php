c563e58db54ccb6851877e596242ba88c83ce96d
boka <web_masters_07@mail.ru>
2017-05-15 13:56:22 +0300

    footer and header

--- wp-content/themes/galvent/index.php
+++ wp-content/themes/galvent/index.php
@@ -1,13 +1,13 @@
 
-<?php require_once('includes/header.php'); ?>
-<?php require_once('includes/header-top-main.php'); ?>
+<?php get_header(); ?>
+
 <?php require_once('includes/equipment.php'); ?>
 <?php require_once('includes/product.php'); ?>
 <?php require_once('includes/about.php'); ?>
 <?php require_once('includes/last.php'); ?>
 <?php require_once('includes/extra.php'); ?>
 <?php require_once('includes/fast.php'); ?>
-<?php require_once('includes/footer.php'); ?>
+<?php get_footer(); ?>
 
 </body>
 </html>
