c8b655d6d6e90f211517d2f14129664ccbb14bcc
engine77 <zaur.1kager@gmail.com>
2017-05-17 19:44:10 +0400

    all file

--- wp-content/themes/galvent/page.php
+++ wp-content/themes/galvent/page.php
@@ -28,32 +28,7 @@
 </div>
 
 
-<div class="container">
-	<h2 class="h2 h2_dark beltconveyors__caption_form">Желаете сделать заказ?</h2>
-	<span class="conveyors__subtext_form">Свяжитесь с нами через форму обратной связи</span>
-	<div class="form_for_inner_page conveyors__marginbottom">
-		<form action="#" method="post" class="js-submit">
-			<div class="form_for_inner_page_box">
-				<span class="for_input">Ваше ФИО:<i>*</i></span>
-				<span class="input input_theme_rosa"><input type="text" class="input__control"  name="uname" placeholder="Василий" required></span> 
-			</div>
-			<div class="form_for_inner_page_box">
-				<span class="for_input">Телефон:<i>*</i></span>
-				<span class="input input_theme_rosa"><input type="tel" class="input__control js-phone" name="phone" placeholder="+7(___)___-__-__" required></span>
-			</div>
-			<div class="form_for_inner_page_box last_box">
-				<span class="for_input">E-mail:<i>*</i></span>
-				<span class="input input_theme_rosa"><input type="email" class="input__control" name="email" placeholder="vasiliy@mail.ru" required></span> 
-			</div>
-			<div class="clear"></div>
-			<span class="for_input for_input_margintop">Комментарий:<i>*</i></span>
-			<textarea name="message" class="message message_theme_rosa" placeholder="Текст комментария"></textarea>
-
-			
-			<span class="conveyors__submit"><input type="submit"></span>
-		</form>
-	</div>
-</div>
+<?php get_template_part( 'content', 'order' ); ?>
 
 
 <?php get_footer(); ?>	
