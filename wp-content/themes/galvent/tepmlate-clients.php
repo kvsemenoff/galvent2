7fdfbde661046aa6cb6918091366bb2ea12dac19
boka <web_masters_07@mail.ru>
2017-05-18 18:30:47 +0300

    client

--- wp-content/themes/galvent/tepmlate-clients.php
+++ wp-content/themes/galvent/tepmlate-clients.php
@@ -13,19 +13,32 @@ Template name: Клиенты
 		</a>
 	</div>
 </div>
-   
-    <div class="clients_content">
-        <div class="container">
-            <div class="clients_header">
-                <h2 class="h2 h2_dark clients_header__h2">Наши клиенты</h2>
-            </div>
-            <div class="clients_block">
-                <div class="col-md-4 col-sm-6 col-lg-3">
-                    <div class="clients_item clients_item__1">
-                        <div class="clients_item__img"></div>
-                        <a href="" class="clients_item__link">ПАО «Газпром»</a>
-                    </div>
-                </div>
+
+<div class="clients_content">
+	<div class="container">
+		<div class="clients_header">
+			<h2 class="h2 h2_dark clients_header__h2">Наши клиенты</h2>
+		</div>
+		<div class="clients_block">
+			<?php $args = array(
+				'post_type' => 'client'
+				);
+
+
+			$query = new WP_Query($args);
+
+			while ( $query->have_posts() ) {
+				$query->the_post(); ?>
+				<div class="col-md-4 col-sm-6 col-lg-3">
+					<div class="clients_item clients_item__1">
+						<div class="clients_item__img"></div>
+						<a href="" class="clients_item__link"><?php the_title(); ?></a>
+					</div>
+				</div>
+
+		<?php }?>
+	</div>
+         <!--    
                 <div class="col-md-4 col-sm-6 col-lg-3">
                     <div class="clients_item clients_item__2">
                         <div class="clients_item__img"></div>
@@ -68,8 +81,8 @@ Template name: Клиенты
                         <a href="" class="clients_item__link">Т плюс Групп</a>
                     </div>
                 </div>
-            </div>
+            -->
         </div>
     </div>
 
-<?php get_footer(); ?>	
+    <?php get_footer(); ?>	
