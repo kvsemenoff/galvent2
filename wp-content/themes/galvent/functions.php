adf3e753eabce3a5132c22afa8be255a1fd95494
engine77 <zaur.1kager@gmail.com>
2017-05-18 11:27:33 +0400

    all file

--- wp-content/themes/galvent/functions.php
+++ wp-content/themes/galvent/functions.php
@@ -596,6 +596,10 @@ class Kama_Breadcrumbs {
 
 }
 
+function wpdocs_custom_excerpt_length( $length ) {
+    return 10;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
 
 
 /**
