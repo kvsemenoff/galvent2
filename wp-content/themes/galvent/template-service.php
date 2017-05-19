fbdb84e960b3913815a58186c9c9613a1f6e3ebb
boka <web_masters_07@mail.ru>
2017-05-19 13:31:24 +0300

    service

--- wp-content/themes/galvent/template-service.php
+++ wp-content/themes/galvent/template-service.php
@@ -25,13 +25,7 @@ Template name: Сервис
 			<div class="clearfix"></div>
 
 			<div class="col-xs-12 col-sm-12 col-md-12">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<?php the_content(); ?>	
-
-		<?php endwhile; ?>
-		<?php else: ?>
-		<?php endif; ?> 
+				
 			</div>
 			<div class="clearfix"></div>
 
