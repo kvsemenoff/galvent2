cc08f0e80b254e57ddf7c1fef9ba0cc33fc78f5a
kvsemenoff <kvsemenoff@mail.ru>
2017-05-31 17:55:27 +0400

    d

--- wp-content/themes/galvent/header.php
+++ wp-content/themes/galvent/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 
-	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="shortcut icon" href="favicon.ico" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/libs.min.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 <?php wp_head(); ?>
