96d4d4bf75799e086c3c505620d187b7aabfd5b2
engine77 <zaur.1kager@gmail.com>
2017-06-05 12:10:45 +0400

    all file

--- wp-content/themes/galvent/footer.php
+++ wp-content/themes/galvent/footer.php
@@ -44,6 +44,30 @@ wp_reset_query();
 				 		'walker'          => '',						
 				 		) );	
 				 		?>
+				</div>
+
+
+				<div class="footer-menu__two">
+					<?php 
+					wp_nav_menu( array(
+						'theme_location'  => 'Main menu',
+						'menu'            => 'Меню футера2', 
+						'container'       => 'div', 
+						'container_class' => '', 
+						'container_id'    => '',
+						'menu_class'      => 'menu_footer', 
+						'menu_id'         => '',
+						'echo'            => true,
+						'fallback_cb'     => 'wp_page_menu',
+						'before'          => '',
+						'after'           => '',
+						'link_before'     => '',
+						'link_after'      => '',
+						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+						'depth'           => 0,
+						'walker'          => '',						
+						) );	
+						?>
 				</div>		
 			
 				<div class="clearfix"></div>
