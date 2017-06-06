e19436b5a4407d3d5f5c30d05f01d1773f02ddb3
engine77 <zaur.1kager@gmail.com>
2017-06-06 17:17:15 +0400

    first commit

--- wp-content/themes/galvent/single.php
+++ wp-content/themes/galvent/single.php
@@ -2,6 +2,9 @@
 
 if (in_category('2') or in_category('3')  or in_category('4')) {
 	get_template_part('single-content');
+}
+else if(in_category('9') ){
+	get_template_part('single-directions');
 } 
 else {
 	get_template_part('single-default.php');
